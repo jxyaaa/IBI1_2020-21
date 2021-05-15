07873489205b0da6219fbf7c35173eca92dd2f98
3200112409bit@10.105.100.153 <3200112409@zju.edu.dn>
2021-05-15 22:53:28 +0800

    initialise

---
+++ practical5/fibonacci.py
@@ -0,0 +1,29 @@
+one=0
+two=1
+current=0
+i=1
+#set initial values
+while i<=12:
+     if i<3:
+             current=1
+     else:
+             current=one+two
+#get the third value
+     print(current)
+     one=two
+     two=current
+     i=i+1
+
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+
---
+++ practical5/variables.py
@@ -0,0 +1,8 @@
+b=190784
+a=101102
+c=290992
+a-c
+d=a-c
+e=a-b
+print(d)
+print(e) 
