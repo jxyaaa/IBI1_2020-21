2a0bbf9ecd5a7cd2ceb68d1527157676593b5f51
3200112409bit@10.105.100.153 <3200112409@zju.edu.dn>
2021-05-17 23:18:12 +0800

    initialise

---
+++ Practical7/covid.py
@@ -0,0 +1,41 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+os.chdir("/Users/jiang/Desktop")
+
+covid_data = pd.read_csv("full_data.csv")
+print(covid_data.iloc[0:12:2,:])
+
+L=[]
+for i in range(0,7996):
+    if covid_data.iloc[i,l] == "Afghanistan":
+        L.append(True)
+    else:
+        L.append(False)
+print(covid_data.loc[L,"total cases”])
+
+world_data = []
+for i in range(0,7996):
+    if covid_data.iloc[i,l] == "World":
+        world_data.append(True)
+    else:
+        world_data.append(False)
+print(covid_data.loc[world_data,"total cases”])
+world_new_cases=covid_data.iloc[world_data,"new_cases"]
+print('the mean of new cases is:',world_new_cases.mean())
+print('the median new cases is:',world_new_cases.median())
+plt.boxplot(world_new_cases)
+plt.xlabel(" new cases")
+plt.ylabel("number")
+plt.title(" the distribution of new cases")
+plt.show()
+
+plt.plot(world_dates,world_new_cases,'b+',label = "world new cases")
+plt.plot(world_dates,world_new_deaths,'r+',label = "world new deaths")
+plt.xticks(world_dates.iloc[0:len(world_dates):4], rotation=-90)
+plt.xlabel("date")
+plt.ylabel("number")
+plt.title(" new cases and new deaths")
+plt.legend()
+plt.show()
