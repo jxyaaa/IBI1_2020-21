f95218ea3734b5cfb9626cf7b59cd9f231ee6746
jxyaaa <84139835+jxyaaa@users.noreply.github.com>
2021-05-18 01:16:27 +0800

    Update r_rate.py

--- practical5/r_rate.py
+++ practical5/r_rate.py
@@ -1,5 +1,13 @@
-r=1.2
+print("the average number of individual infected by individual with the virus")
+r=input()
+r=float(r) 
 n=84
-g=5
-#5 generations
-print(n*r**g)
+x=0 
+# generation
+
+while x<5:
+    n=n*r+n 
+#each round n people inferct n*r people,and plus original n people
+    x=x+1 
+#round
+print("r="+str(r)+" n="+str(n))
