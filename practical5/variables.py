1935399f0a8383fa961844a4fd817c5e2d975f08
jxyaaa <84139835+jxyaaa@users.noreply.github.com>
2021-05-18 00:57:21 +0800

    Update variables.py

--- practical5/variables.py
+++ practical5/variables.py
@@ -1,3 +1,4 @@
+#task 3.1
 b=190784
 a=101102
 c=290992
@@ -13,5 +14,16 @@ elif d>=e:
   print("d>e")
 else :
   print("d<e")
+  
+  
+#task 3.2
+print("set X as ture or false")
+X=input()
+print("set Y as ture or false")
+Y=input()
+Z=(X and not Y) or (Y and not X)
+W=X!=Y
+print("Z",Z)
+print("W",W)  
   
   
